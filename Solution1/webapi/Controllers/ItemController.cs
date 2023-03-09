0744b9bf45b285b743322fd15d17faee54aa9b77
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-03-08 16:34:40 -0800

    Items to ItemController.

--- Solution1/webapi/Controllers/ItemController.cs
+++ Solution1/webapi/Controllers/ItemController.cs
@@ -9,7 +9,12 @@ namespace webapi.Controllers
     {
         private static readonly IEnumerable<ItemModel> items = new[]
         {
-            new ItemModel{Id=0, Title="Joe Mamma"}
+            new ItemModel{Id=0, Title="Bob"},
+            new ItemModel{Id=1, Title="Joe"},
+            new ItemModel{Id=2, Title="Mary"},
+            new ItemModel{Id=3, Title="Emma"},
+            new ItemModel{Id=4, Title="Jamie"},
+            new ItemModel{Id=5, Title="Jeff"}
         };
     }
 }
