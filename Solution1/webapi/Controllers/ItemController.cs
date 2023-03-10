7b18baa319d7eeecdcc75da3bda4348209f9d0ae
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-03-09 17:01:51 -0800

    More names.

--- Solution1/webapi/Controllers/ItemController.cs
+++ Solution1/webapi/Controllers/ItemController.cs
@@ -9,12 +9,17 @@ namespace webapi.Controllers
     {
         private static readonly IEnumerable<ItemModel> Items = new[]
         {
-            new ItemModel{Id=0, Title="Bob"},
-            new ItemModel{Id=1, Title="Joe"},
-            new ItemModel{Id=2, Title="Mary"},
+            new ItemModel{Id=0, Title="Bryce"},
+            new ItemModel{Id=1, Title="Henry"},
+            new ItemModel{Id=2, Title="Nathan"},
             new ItemModel{Id=3, Title="Emma"},
             new ItemModel{Id=4, Title="Jamie"},
-            new ItemModel{Id=5, Title="Jeff"}
+            new ItemModel{Id=5, Title="Ryan"},
+            new ItemModel{Id=6, Title="Johnny"},
+            new ItemModel{Id=7, Title="David"},
+            new ItemModel{Id=8, Title="Elizabeth"},
+            new ItemModel{Id=9, Title="Mohsin"},
+            new ItemModel{Id=10, Title="Joel"}
         };
         [HttpGet(Name = "GetItems")]
         public IEnumerable<ItemModel> Get()
